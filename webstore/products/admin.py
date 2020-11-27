c580afeb390e1fa5b7c6048a309322f9cacbddb1
Chaplygin-vlad <metro_flava@mail.ru>
2020-11-27 16:31:17 +0300

    complete model and register in admin

--- webstore/products/admin.py
+++ webstore/products/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import *
+
+
+# Прописываем условия, что бы при создании, например, наушников, можно было выбрать только категорию "Наушники"
+# TODO
+class EarphoneCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class EarphonesAdmin(admin.ModelAdmin):
+
+    def formfield_from_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return EarphoneCategoryChoiceField(Category.objects.filter(slug='earphone'))
+        return super().formfield_for_choice_field(db_field, request, **kwargs)
+
+
+class PowerBankCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class PowerBankAdmin(admin.ModelAdmin):
+
+    def formfield_from_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return PowerBankCategoryChoiceField(Category.objects.filter(slug='power_bank'))
+        return super().formfield_for_choice_field(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Earphones, EarphonesAdmin)
+admin.site.register(PowerBank, PowerBankAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
--- webstore/products/models.py
+++ webstore/products/models.py
@@ -28,14 +28,14 @@ class Category(models.Model):
         return self.name
 
 
+# базовая модель продукта от которой наследуются все остальные категории
 class Product(models.Model):
-
     class Meta:
         abstract = True
 
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True)  # endpoint для роутинга url
     image = models.ImageField(verbose_name='Изображение')
     description = models.TextField(verbose_name='Описание')
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
@@ -44,13 +44,52 @@ class Product(models.Model):
         return self.title
 
 
+class Earphones(Product):
+    model_type = models.CharField(max_length=255, verbose_name='Тип модели')
+    connect_type = models.CharField(max_length=255, verbose_name='Тип подключения')
+    microphone = models.BooleanField(default=False, verbose_name='Наличие микрофона')
+    noise_suppression = models.BooleanField(default=False, verbose_name='Наличие шумоизоляции')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аккумулятора')
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
+
+
+class PowerBank(Product):
+    accumulator_type = models.CharField(max_length=255, verbose_name='Тип аккумулятора')
+    ac_capacity = models.CharField(max_length=255, verbose_name='Емкость аккумулятора')
+    out_amperage = models.CharField(max_length=255, verbose_name='Выходное напряжение')
+    input_connectors = models.CharField(max_length=255, verbose_name='Входные разъемы')
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
+
+
+
+
+# Earphones
+# type
+# connect_type
+# microphone
+# noise suppression
+
+# Powerbank
+# type
+# capacity
+# amperage
+# input_connectors
+
+
 # Объект корзины
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     # С помощью взаимосвязи с корзиной можем получить информацию, к какой корзине привязан данный продукт корзины
     # например cart.related_products.all()
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     quantity = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Сумма')
 
@@ -77,6 +116,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"Покупатель: {self.user.first_name} {self.user.last_name}"
-
-
-    
