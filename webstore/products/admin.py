489b91367265083275a81d6581c2b1d1e381abff
Chaplygin-vlad <metro_flava@mail.ru>
2020-12-03 16:23:05 +0300

    add help text in admin model form for upload image and function for raise validation error

--- webstore/products/admin.py
+++ webstore/products/admin.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
 from django import forms
+from django.forms import ModelForm, ValidationError
 
 from .models import *
 
+from PIL import Image
+
+
+class EarphoneAdminForm(ModelForm):
+    MIN_RESOLUTION = (400, 400)
+
+    # добавление вспомогательного текста в админке при загрузке изображений
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загружайте изображния с минимальным расшмрением {}x{}'.format(
+            *self.MIN_RESOLUTION)
+
+    # функция для ограничения по загрузке изображений
+    def clean_image(self):
+        # получение изображения из формы
+        image = self.cleaned_data['image']
+        # отрытие изображения для получения его параметров
+        img = Image.open(image)
+        # получаем минимальные размеры
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+
 
 # Прописываем условия, что бы при создании, например, наушников, можно было выбрать только категорию "Наушники"
-# TODO
 
 class EarphoneAdmin(admin.ModelAdmin):
+    form = EarphoneAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
